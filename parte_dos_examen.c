f336592a297a45a028598b3f61a5ebcbaf44bc72
marinolaz <88785706+marinolaz@users.noreply.github.com>
2021-09-09 13:59:01 -0500

    Add files via upload

---
+++ parte_dos_examen.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	float arr[13]={3.3, 4.5, 5.0, 3.4, 4.2, 5.3, 3.2, 4.3, 5.2, 7.1, 3.3, 4.8, 5.2};
+	int i;
+	
+	switch (i)
+{
+case 1:
+printf("Imperceptible, sin problema");
+break;
+
+case 2:
+printf("Sin problemas, avisar");
+break;
+
+case 3:
+printf("Emitir alerta, desalojar");
+break;
+
+case 4:
+printf("Emitir alerta, enviar equipos de apoyo");
+break;
+
+case 5:
+printf("Emitir alerta, enviar equipos de rescate");
+break;
+
+case 6:
+printf("Emergencia nacional, solicitar apoyo externo");
+break;
+
+}
+
+return 0;
+}
+
