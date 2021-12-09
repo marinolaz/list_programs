f2bddd5e80a87ae53054ab9b20bccc5a38f45863
marinolaz <88785706+marinolaz@users.noreply.github.com>
2021-12-09 11:00:06 -0600

    Add files via upload

---
+++ sandwichsumres.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main (void)
+{
+int num1,num2;
+float res;
+int d;
+printf("indica el primer valor:  ");
+scanf("%d",&num1);
+printf("indica el segundo valor:  ");
+scanf("%d",&num2);
+
+printf("indica la operacion que quiere realizar\n 1) suma\n 2) resta\n 3)multiplicacion\n 4)division       :");
+scanf("%d",&d);
+
+res=0;
+switch(d)
+{
+case 1:
+res=num1+num2;
+break;
+
+case 2:
+res=num1-num2;
+break;
+
+case 3:
+res=num1*num2;
+break;
+
+case 4:
+res=(float)num1/(float)num2;
+break;
+
+default:
+printf("opcion invalida");
+}
+printf("el resultado es:  %.3f\n",res);
+
+return 0;
+}
